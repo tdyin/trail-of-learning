de79c91cf83da26d06e75212070597e35567b2db
yiine <100390112+yiine@users.noreply.github.com>
2022-03-05 17:58:37 -0800

    Delete rust-tm directory

--- rust-tm/target/rls/debug/deps/rust_tm-74bd7913e9713add.d
+++
@@ -1,6 +0,0 @@
-/Users/yin/Dev/rust-tm/target/rls/debug/deps/rust_tm-74bd7913e9713add.rmeta: src/main.rs src/cli.rs
-
-/Users/yin/Dev/rust-tm/target/rls/debug/deps/rust_tm-74bd7913e9713add.d: src/main.rs src/cli.rs
-
-src/main.rs:
-src/cli.rs:
--- rust-tm/target/rls/debug/deps/rust_tm-bc4403055f5ede06.d
+++
@@ -1,6 +0,0 @@
-/Users/yin/Dev/rust-tm/target/rls/debug/deps/rust_tm-bc4403055f5ede06.rmeta: src/main.rs src/cli.rs
-
-/Users/yin/Dev/rust-tm/target/rls/debug/deps/rust_tm-bc4403055f5ede06.d: src/main.rs src/cli.rs
-
-src/main.rs:
-src/cli.rs:

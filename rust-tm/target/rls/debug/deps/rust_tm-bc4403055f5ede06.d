5c0244167ed253371efc8f850c5009184c1d4c54
yiine <100390112+yiine@users.noreply.github.com>
2022-03-05 17:58:02 -0800

    Rust fundamental from Traversy Media

---
+++ rust-tm/target/rls/debug/deps/rust_tm-bc4403055f5ede06.d
@@ -0,0 +1,6 @@
+/Users/yin/Dev/rust-tm/target/rls/debug/deps/rust_tm-bc4403055f5ede06.rmeta: src/main.rs src/cli.rs
+
+/Users/yin/Dev/rust-tm/target/rls/debug/deps/rust_tm-bc4403055f5ede06.d: src/main.rs src/cli.rs
+
+src/main.rs:
+src/cli.rs:

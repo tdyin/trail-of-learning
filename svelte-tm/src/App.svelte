7c63c13eb50b2afed94412e02bd4d9282dcc64e1
yiine <100390112+yiine@users.noreply.github.com>
2022-05-21 12:01:54 -0700

    Add files via upload

---
+++ svelte-tm/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import FeedbackForm from './components/FeedbackForm.svelte'
+  import FeedbackList from './components/FeedbackList.svelte'
+  import FeedbackStas from './components/FeedbackStats.svelte'
+
+  const addFeedback = (e) => {
+    const newFeedback = e.detail
+    feedback = [newFeedback, ...feedback]
+  }
+</script>
+
+<main class="container">
+  <FeedbackForm />
+  <FeedbackStas />
+  <FeedbackList />
+</main>
